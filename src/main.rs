8c089c170660e0eefe8f0bfcb2a1a7de9a884218
Smartlinuxcoder <smartcoder@smartlinux.xyz>
2024-12-01 19:42:15 +0100

    FINALLY SOMETHING THAT WORKS

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
 mod day1;
 
+use std::time::Instant;
 
 fn main() {
     println!("Hello, world!");
+    
+    // Benchmark the `day1::main::main()` function
+    let start = Instant::now();
+
     day1::main::main();
+
+    let duration = start.elapsed();
+    println!("Execution time of day1::main::main: {:?}", duration);
 }
